2ebc8dd6cd8c2dd33df355c657c79f93a6d355f0
dnaniel213 <dnaniel213@gmail.com>
2023-03-14 15:13:33 +0800

    - new assets 1

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class CamFollow : MonoBehaviour {
     
-    public float dampTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
     public Transform target = null;
+    [SerializeField] private float smoothTime = 0.3f;
+    [SerializeField] private float minX = -10f;
+    [SerializeField] private float maxX = 10f;
 
     // Update is called once per frame
-    void FixedUpdate () 
+    void LateUpdate () 
     {
         if(Global.guiState != GUISTATE.game)
         {
@@ -21,10 +23,10 @@ public class CamFollow : MonoBehaviour {
         
         if (target != null)
         {
-            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-            Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.3f, 10f)); //(new Vector3(0.5, 0.5, point.z));
-            Vector3 destination = transform.position + delta;
-            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+            Vector3 targetPosition = target.position;
+            targetPosition.z = transform.position.z;
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         }
     
     }
