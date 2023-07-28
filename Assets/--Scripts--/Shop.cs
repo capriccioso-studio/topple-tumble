18e4676884761cd6721b94514b671f44129e011e
Riasnner <73977565+Riasnner@users.noreply.github.com>
2023-07-28 14:41:36 +0800

    Added function to display shop items

--- Assets/--Scripts--/Shop.cs
+++ Assets/--Scripts--/Shop.cs
@@ -5,26 +5,74 @@ using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
-    GameObject ItemTemplate;
-    GameObject ItemTemplate2; //this script duplicates the items to 15 times
-    GameObject g;
-    GameObject h;
-    [SerializeField] Transform ShopScrollView;
-    [SerializeField] Transform ShopScrollView2;
+    public GameObject SeedItemTemplate, PlatformItemTemplate; //this script duplicates the items to 15 times
+    GameObject seedItem, platformItem;
+    public SeedItems[] seed;
+    public PlatformItems[] platform;
+
+    [SerializeField] Transform SeedShopScrollView;
+    [SerializeField] Transform PlatformShopScrollView;
 
     void Start()
     {
-         ItemTemplate2 = ShopScrollView2.GetChild (0).gameObject;
+        SeedItemTemplate = SeedShopScrollView.GetChild (0).gameObject;
+        PlatformItemTemplate = PlatformShopScrollView.GetChild (0).gameObject;
 
-        ItemTemplate = ShopScrollView.GetChild (0).gameObject;
-
-        for(int i = 0; i<15; i++)
+        foreach(SeedItems seed in seed)
         {
-            h = Instantiate (ItemTemplate2, ShopScrollView2);
-            g = Instantiate (ItemTemplate, ShopScrollView);
+            seedItem = Instantiate (SeedItemTemplate, SeedShopScrollView);
+            // seed.itemRef = seedItem;
+
+            seedItem.transform.GetComponent<Button>().onClick.AddListener(() => {
+                BuyItem(seed);
+            });
+            
+            // foreach(Transform child in seedItem.transform){
+            //     if(child.gameObject.name = "price")
+            //     {
+            //         child.gameObject.GetComponent<Text>().text = seed.cost.ToString();
+            //     }else if(child.gameObject.name = "seed"){
+            //         child.gameObject.GetComponent<Image>().sprite = seed.image;
+            //     }
+            // }
         }
 
-        Destroy  (ItemTemplate);
-        Destroy  (ItemTemplate2);
+        foreach(PlatformItems platform in platform)
+        {
+            platformItem = Instantiate (PlatformItemTemplate, PlatformShopScrollView);
+        }
+
+        // for(int i = 0; i<15; i++)
+        // {
+        //     seedItem = Instantiate (SeedItemTemplate, SeedShopScrollView);
+        //     platformItem = Instantiate (PlatformItemTemplate, PlatformShopScrollView);
+            
+        // }
+
+        Destroy  (SeedItemTemplate);
+        Destroy  (PlatformItemTemplate);
     }
+
+    public void BuyItem(SeedItems seed){
+        Debug.Log("Clicked");
+        if(Global.orb >= seed.cost){
+            Global.orb -= seed.cost;
+        }
+    }
+}
+
+[System.Serializable]
+public class SeedItems{
+    public Sprite image;
+    public int cost;
+    public SeedScriptableObject seed;
+    [HideInInspector] public GameObject itemRef;
+}
+
+[System.Serializable]
+public class PlatformItems{
+    public Sprite image;
+    public int cost;
+    public PlatformScriptableObject platform;
+    [HideInInspector] public GameObject itemRef;
 }
