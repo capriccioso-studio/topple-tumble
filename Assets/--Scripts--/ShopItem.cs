c1865f67157893a2b5e129425e3980b54fd72248
Riasnner <73977565+Riasnner@users.noreply.github.com>
2023-07-31 18:03:24 +0800

    Button changes to bought when pressed, no longer interactable

--- Assets/--Scripts--/ShopItem.cs
+++ Assets/--Scripts--/ShopItem.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopItem : MonoBehaviour
 {
-    public SeedScriptableObject seed;
-    public PlatformScriptableObject platform;
+    public bool isItemBought;
+    public Button button;
+    public TMP_Text text;
 
-    public void EquipSeed(){
-        Global.seedtype = seed;
-    }
-
-    public void EquipPlatform(){
-        Global.platformtype = platform;
+    public void OnButtonPress()
+    {
+        //test code
+        if(Global.orb > 0){
+            button.interactable = false;
+            isItemBought = true;
+            button.transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = "BOUGHT";
+        }
     }
 }
