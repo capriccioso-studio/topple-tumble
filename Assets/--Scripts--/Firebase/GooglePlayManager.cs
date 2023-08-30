dcbeb683650340af1faace8bbc016c49903b9f9f
Riasnner <ashleymturla@gmail.com>
2023-08-30 20:37:43 +0800

    Added google play leaderboard

--- Assets/--Scripts--/Firebase/GooglePlayManager.cs
+++ Assets/--Scripts--/Firebase/GooglePlayManager.cs
@@ -3,38 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
-using UnityEngine.UI;
-using TMPro;
 using UnityEngine.SocialPlatforms;
 
 public class GooglePlayManager : MonoBehaviour
 {
     public bool connected;
-    public TMP_Text test;
 
     private void Start()
     {
         PlayGamesPlatform.Activate();
         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
-        
     }
 
     internal void ProcessAuthentication(SignInStatus status) {
-      if (status == SignInStatus.Success) {
-        test.text = "Connected to Google Play";
-      } else {
-        test.text = "Did not connect to Google Play";
-      }
-    }
-
-    public void SignInWithGoogleAsync()
-    {
-        PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
+      connected = (status == SignInStatus.Success)? true : false;
     }
 
     public void ShowLeaderboard()
     {
+      if(!connected){
+        PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
+      }
       Social.ShowLeaderboardUI();
+    }
+
+    public void SubmitToLeaderboard()
+    {
+      Social.ReportScore(Global.score, "CgkIqcOlxe4JEAIQAA", (bool success) =>{
+        Debug.Log(success);
+      });
     }
 }
 
